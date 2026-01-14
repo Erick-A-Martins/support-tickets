0f29793a32188088e24827f964d0ffdfc7167abf
Erick-A-Martins <erickalvesqmartins@gmail.com>
2026-01-14 11:57:59 -0300

    handle request parameters

--- src/controllers/tickets/index.js
+++ src/controllers/tickets/index.js
@@ -1,5 +1,9 @@
 
 export function index({ request, response, database }){
+    const { status } = request.query;
+
+    console.log(price);
+
     const tickets = database.select("tickets");
     
     return response.end(JSON.stringify(tickets));
